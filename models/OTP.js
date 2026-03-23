5e2222655740be7b4f2d72db2f5767afe3c6ce19
Samuel Popoola <samuelpopoola323@gmail.com>
2026-03-23 20:07:15 +0100

    Update OTP.js

--- models/OTP.js
+++ models/OTP.js
@@ -4,7 +4,8 @@ const otpSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   otpHash: { type: String, required: true },
   expiresAt: { type: Date, required: true },
-  attempts: { type: Number, default: 0 }
+  attempts: { type: Number, default: 0 },
+  lastSentAt: { type: Date }
 }, { timestamps: true });
 
 export default mongoose.model("OTP", otpSchema);
