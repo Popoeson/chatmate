1e0fd7bebaad43751d37f8cd730bfc27d9981870
Samuel Popoola <samuelpopoola323@gmail.com>
2026-03-23 12:53:33 +0100

    Create user.js

---
+++ user.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  passwordHash: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  registrationStep: { type: String, default: "form_submitted" } // form_submitted | otp_pending | verified
+}, { timestamps: true });
+
+export default mongoose.model("User", userSchema);
