91c7cd246e13d52e2c911468dbb940f2ebeef251
Samuel Popoola <samuelpopoola323@gmail.com>
2026-03-23 12:54:25 +0100

    Create OTP.js

---
+++ OTP.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  otpHash: { type: String, required: true },
+  expiresAt: { type: Date, required: true },
+  attempts: { type: Number, default: 0 }
+}, { timestamps: true });
+
+export default mongoose.model("OTP", otpSchema);
