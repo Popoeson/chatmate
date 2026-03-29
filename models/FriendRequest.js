0f2118d711bf1776ed9ce14548e8fa0166facaff
Samuel Popoola <samuelpopoola323@gmail.com>
2026-03-29 08:29:45 +0100

    Update FriendRequest.js

--- models/FriendRequest.js
+++ models/FriendRequest.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const FriendRequestSchema = new mongoose.Schema({
   requester: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  status: { type: String, enum: ['pending', 'accepted'], default: 'pending' },
+  status: { type: String, enum: ['pending', 'accepted', 'blocked'], default: 'pending' },
 }, { timestamps: true });
 
 export default mongoose.model('FriendRequest', FriendRequestSchema);
