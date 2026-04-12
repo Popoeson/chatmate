e6f6a128944baa3861977064005e5d5b8ca4e195
Samuel Popoola <samuelpopoola323@gmail.com>
2026-04-12 22:03:38 +0100

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -6,6 +6,11 @@ const messageSchema = new mongoose.Schema({
   text:      { type: String, required: true },
   delivered: { type: Boolean, default: false },
   read:      { type: Boolean, default: false },
+  replyTo: {
+    messageId: { type: mongoose.Schema.Types.ObjectId, ref: "Message", default: null },
+    text:      { type: String, default: null },
+    sender:    { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null }
+  }
 }, { timestamps: true });
 
 export default mongoose.model("Message", messageSchema);
